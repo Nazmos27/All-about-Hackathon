bb79f30b8edbc9fe53d72887e0403e940986dad2
Nazmos27 <nazmos789@gmail.com>
2024-02-02 21:57:05 +0600

    commiting mock test solution

---
+++ Team Fragile/ApiWithSqlite-practice/table.js
@@ -0,0 +1,9 @@
+
+
+const sqlite = require("sqlite3").verbose();
+const db = new sqlite.Database("./bookStore.db", sqlite.OPEN_READWRITE, (err)=>{
+    if(err) return console.error(err);
+})
+
+const sql = `CREATE TABLE bookStore(ID INTEGER PRIMARY KEY, title TEXT, author TEXT, genre TEXT, price REAL)`;
+db.run(sql);
---
+++ Team Fragile/readme.txt
@@ -0,0 +1,7 @@
+Team Name : Team Fragile
+Institution name : Jagannath University
+
+Email List : 
+1st participant : nazmos789@gmail.com
+2nd participant : niloyp10@gmail.com
+3rd participant :  alauddinajad13@gmail.com
